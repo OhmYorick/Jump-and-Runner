bbc298a9ccf99717aa34cf77f407f9810d21fe93
OhmYorick <64013584+OhmYorick@users.noreply.github.com>
2021-03-25 09:38:12 +0100

    Initial commit

---
+++ src/main/java/JumpAndRunner/Main.java
@@ -0,0 +1,15 @@
+package JumpAndRunner;
+
+import nl.han.ica.oopg.engine.GameEngine;
+
+public class Main extends GameEngine {
+    @Override
+    public void setupGame() {
+
+    }
+
+    @Override
+    public void update() {
+
+    }
+}
---
+++ src/main/java/JumpAndRunner/Player.java
@@ -0,0 +1,17 @@
+package JumpAndRunner;
+
+import nl.han.ica.oopg.objects.GameObject;
+import processing.core.PGraphics;
+
+public class Player extends GameObject {
+
+    @Override
+    public void update() {
+
+    }
+
+    @Override
+    public void draw(PGraphics g) {
+
+    }
+}
